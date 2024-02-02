4a3593519abb5b3587e6e8cdcd912382faf86f27
LiamF76 <148378361+LiamF76@users.noreply.github.com>
2024-02-02 16:30:41 -0500

    small edit
    made one things have *stars*!

--- demo.py
+++ demo.py
@@ -3,18 +3,21 @@ from random import *
 from easygui import *
 
 def angelinaGreeting():
+    #define the title of the box, the msg it shows, and the choices in buttons
     title = "Angelina, Triton (she/her):"
     msg = "Will you deliver this letter?\nIt has to get to the Mayor TONIGHT!"
     choice = ['Yes!', 'No.']
 
-    user = buttonbox(msg,title,choice)
+    user = choicebox(msg,title,choice)
 
-    msg2 = "Thank you! I trust you do do this VERY DANGEROUS thing!!!"
+    msg2 = ("Thank you! I trust you do do this VERY DANGEROUS thing!!!\n"
+            "The Mayor's office is in the *Aquamarine District*, hurry!")
 
     if user == 'Yes!':
         msgbox(msg2,title)
-    else:
-        msgbox('wtf bro',title)
+    elif user == 'No.':
+        msgbox('No! We need your help please! Do it for the people of Neptune!',title)
+        angelinaGreeting()
 
 def firstRoom():
     angelinaGreeting()
